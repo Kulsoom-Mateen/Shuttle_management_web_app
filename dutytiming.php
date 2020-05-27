9a0681f2cee5a385cff232d0853e077f00aedb5d
Kulsoom-Mateen <kulsoommateen07@gmail.com>
2020-05-27 19:56:52 -0400

    Added php file for driver & conductor duty timings

--- dutytiming.php
+++ dutytiming.php
@@ -185,7 +185,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="topnav">
         <img src="logo.png"
             style="height:90px;width:90px;float:left;margin-top:5px;margin-left:5px;">
-        <p class="para"><b>NED University Shuttle service</b></p>
+        <p class="para"><b>Shuttle service Web App</b></p>
     </div>
     <nav class="navbar">
         <span class="open-slide">
